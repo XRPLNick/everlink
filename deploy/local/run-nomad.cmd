dd4c208830d4b7ab839dcda5c3c27a78e22663e3
NickFields0101 <NickFields0101@users.noreply.github.com>
2026-09-03 00:15:21 +0000

    hpdevkit as a dev dependency; drop the old desktop launcher

--- deploy/local/run-nomad.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0nomad-connector\deploy\local\run.ps1"
-echo.
-echo (this window closes in 15 minutes; full log in nomad-connector\deploy\local\out\run.log)
-timeout /t 900
